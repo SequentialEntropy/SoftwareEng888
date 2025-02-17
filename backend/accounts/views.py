357e92c65bedcf7d8ec46740271ba19de46112e4
SequentialEntropy <asahi79289@gmail.com>
2025-02-17 01:12:38 +0000

    CRUD user profiles

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,10 +1,48 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer, UserProfileSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import UserProfile
+
+from rest_framework import viewsets
 
 # Create your views here.
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
+class UserProfileListCreate(generics.ListCreateAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return UserProfile.objects.filter(user=user)
+    
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(user=self.request.user)
+        else:
+            print(serializer.errors)
+
+class UserProfileDelete(generics.DestroyAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = IsAuthenticated
+
+    def get_queryset(self):
+        user = self.request.user
+        return UserProfile.objects.filter(user=user)
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+# class UserViewSet(viewsets.ModelViewSet):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#     permission_classes = [IsAuthenticated]
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
