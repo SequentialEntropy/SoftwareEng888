92a3d4463c27f736ea6d3290e136a79615a6caa6
SequentialEntropy <asahi79289@gmail.com>
2025-03-05 15:35:46 +0000

    Restore ranked-users accidentally deleted by 210943d876924fba89950b7092450cfffc67b788

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('me/', views.UserDetailView.as_view(), name="user-detail")
+    path('me/', views.UserDetailView.as_view(), name="user-detail"),
+    path("ranked-users/", views.RankedUsersView.as_view(), name="ranked-users")
 ]
