2b655d70c9f5602c1a92e018e74f8b292dc88879
SequentialEntropy <asahi79289@gmail.com>
2025-03-06 04:16:04 +0000

    Make Square.jsx more readable

--- application/src/components/Square.jsx
+++ application/src/components/Square.jsx
@@ -1,15 +1,21 @@
 import styles from "../styles/Board.module.css"
 
 export default function Square({id, name, backgroundColor}, squareRefs) {
+    const isStartSquare = id === 0
+
     return (
-        <div className={styles.item} key={id} ref={e => {squareRefs.current[id] = e}} style={id === 0 ? {backgroundColor} : {}}>
-            {id === 0 ? (
-                <h3 style={{color: '#d9d9d9', fontSize: '50px', transform: 'rotate(-25deg)', margin:'auto', letterSpacing: '5px'}}>START</h3>
-            ) : (
+        isStartSquare ? (
+            <div className={styles.item} key={id} ref={e => {squareRefs.current[id] = e}} style={id === 0 ? {backgroundColor} : {}}>
+                <h3 style={{color: '#d9d9d9', fontSize: '50px', transform: 'rotate(-25deg)', margin:'auto', letterSpacing: '5px'}}>
+                    START
+                </h3>
+            </div>
+        ) : (
+            <div className={styles.item} key={id} ref={e => {squareRefs.current[id] = e}}>
                 <div className={styles.tile_bar} style={{backgroundColor: backgroundColor}}>
                     <h3>{name}</h3>
                 </div>
-            )}
-        </div>
+            </div>
+        )
     )
 }
