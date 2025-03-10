a450967eec608bf718f3d5f94e3fe81eb9950986
SequentialEntropy <asahi79289@gmail.com>
2025-03-10 02:34:59 +0000

    Use disabled styling for Chance button

--- application/src/components/Chance.jsx
+++ application/src/components/Chance.jsx
@@ -2,10 +2,15 @@ import styles from "../styles/Board.module.css"
 
 export default function Chance({setShowChance, getChance, showChance}) {
     return (
-        <div className={styles.chance_deck}>
+        <div className={styles.chance_deck} style={{opacity: getChance ? 1 : 0.5}}>
             {/* Chance Card Button */}
 
-            <button className={styles.task_btn} onClick={() => setShowChance(true)} disabled = {!getChance}>Chance</button>
+            <button
+                className={styles.task_btn}
+                onClick={() => setShowChance(true)}
+                disabled = {!getChance}
+                style={{cursor: getChance ? "pointer" : "not-allowed"}}
+            >Chance</button>
 
             {/* Chance Card Popup - only available after landing on 6 */}
             {getChance && showChance && (
