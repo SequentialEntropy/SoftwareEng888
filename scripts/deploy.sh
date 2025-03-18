6cc8d8b8cef32b7f4595a8af9d1d0adb1f606ac6
SequentialEntropy <asahi79289@gmail.com>
2025-03-18 21:40:56 +0000

    Add auto-deploy using deploy.yml and deploy.sh

---
+++ scripts/deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Stop server
+sudo systemctl stop SoftwareEng888-frontend.service
+sudo systemctl stop SoftwareEng888-backend.service
+
+# Pull and apply latest changes
+cd /root/SoftwareEng888
+git pull origin main
+
+# Install any new python packages
+source ./venv/bin/activate
+python3 -m pip install -r requirements.txt
+
+# Apply new migrations
+cd backend
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+# Install any new node packages
+cd ../application
+npm install
+
+# Start server
+sudo systemctl start SoftwareEng888-frontend.service
+sudo systemctl start SoftwareEng888-backend.service
