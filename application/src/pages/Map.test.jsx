64bfdd7b7c6750e4830032dbaa2bfe2664f44b4c
GarethSRGKZY <divinegareth1313@gmail.com>
2025-03-20 21:34:20 +0800

    Create Map.test.jsx to test Map.jsx functionalities

---
+++ application/src/pages/Map.test.jsx
@@ -0,0 +1,33 @@
+/**
+ * Map.test.jsx - A React component for testing the functionalities of the map page.
+ * 
+ * @file Handles rendering of the map page and tests the map page functions. 
+ * @author Gareth Zheng Yang Koh
+ * @author Crystal Tsui
+ * @version 1.2.0 
+ * @since 20-03-2025
+*/
+
+/**
+ * Component Render tests
+ * 1. Render map component
+ * 
+ * API tests
+ * 1. Test task data is displayed correctly
+ * 2. Test handling API error
+ * 
+ * Geolocation tests
+ * 1. Test display of user location when geolocation is available
+ * 2. Test handling geolocation error
+ * 
+ * Map Render tests
+ * 1. Test map render with correct coordinates
+ * 2. Test marker render on map
+ * 
+ * Integration tests
+ * 1. Test display task info when task is loaded
+ * 2. Test display no task if current task is not found
+ * 
+ * Responsiveness tests
+ * 1. Test page on different dimension
+ */
