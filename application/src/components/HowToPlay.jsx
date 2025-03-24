d0dd373c0d25d1784021f93fe793aa8960953105
carinaj16 <129616579+carinaj16@users.noreply.github.com>
2025-03-24 14:41:42 +0000

    Update HowToPlay.jsx
    Refined how to play instructions

--- application/src/components/HowToPlay.jsx
+++ application/src/components/HowToPlay.jsx
@@ -14,7 +14,7 @@ function HowToPlay() {
                         <div className={styles.how_to_play_container3}>
                             <h2 className={styles.how_to_play_title}>How to Play</h2>
                         </div>
-                        <p className={styles.how_to_play_instructions}>1. Spin the wheel <br></br>2. Do task at specified location <br></br>3. Click OK once task is completed <br></br> 4. Get points and view your position on the leaderboard</p>
+                        <p className={styles.how_to_play_instructions}>1. Spin the wheel <br></br>2. Do task at specified location <br></br>3. Claim points <br></br> 4. View your position on the leaderboard and battle your friends!</p>
                         <button
                         className={styles.exit_btn}
                         onClick={() => setShowHowToPlay(false)}>x
