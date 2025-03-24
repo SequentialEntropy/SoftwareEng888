a7c0fa8f44b89cee41fffea1494a75b5c44e743e
AmreetD <ad1029@exeter.ac.uk>
2025-03-24 16:33:06 +0000

    Attempt to fix mobile checkbox

--- backend/accounts/management/commands/populatedefaultchances.py
+++ backend/accounts/management/commands/populatedefaultchances.py
@@ -5,7 +5,13 @@ class Command(BaseCommand):
     help = "Populate the database with default chance cards"
 
     def handle(self, *args, **kwargs):
-        Chance.objects.create(description="Your trees grew!", score_to_award=5)
+        Chance.objects.create(description="Bonus 5 points!", score_to_award=5)
+        Chance.objects.create(description="Bonus 10 points !!", score_to_award=10)
+        Chance.objects.create(description="Bonus 15 points", score_to_award=15)
+        Chance.objects.create(description="Oh No! -5 points", score_to_award=-5)
+        Chance.objects.create(description="Oh No! -10 points", score_to_award=-10)
+        Chance.objects.create(description="Oh No! -15 points", score_to_award=-15)
+
 
         count = Chance.objects.count()
         self.stdout.write(self.style.SUCCESS(f'Successfully added {count} entries to Chance'))
