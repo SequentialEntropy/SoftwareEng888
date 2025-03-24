22bb9fcbe2be2da04a42a968ad8d30c101a092e9
SequentialEntropy <asahi79289@gmail.com>
2025-03-24 21:15:44 +0000

    Revert admin tasks editor to use checkboxes

--- application/src/components/Navbar.jsx
+++ application/src/components/Navbar.jsx
@@ -1,10 +1,24 @@
 import { Link } from "react-router-dom"
 import styles from "../styles/Navbar.module.css"
 import DisplayToggle from "./DisplayToggle"
+import { useEffect, useState } from "react"
+import api from "../api"
 
 
 
 function NavBar(){
+    const [isAdmin, setIsAdmin] = useState(false)
+
+    const adminCheck = async () => {
+        const accountDetails = await api.get("/accounts/me/")
+        if (accountDetails.data.is_staff) {
+            setIsAdmin(true)
+        }
+    }
+
+    useEffect(() => {
+        adminCheck()
+    }, [])
     return (
         <div>
             {/* Sidebar navigation */}
@@ -17,7 +31,9 @@ function NavBar(){
                     <Link to="/board"><i className="bi bi-dice-3-fill"  ></i></Link>
                     <Link to="/map"><i className="bi bi-map-fill"  ></i></Link>
                     <Link to="/profile"><i className="bi bi-person-circle"  ></i></Link>
-                    <Link to="/"><i className="bi bi-person-fill-lock"  ></i></Link>
+                    {isAdmin && (
+                        <Link to="/admin"><i className="bi bi-person-fill-lock"  ></i></Link>
+                    )}
                     <Link to="/logout"><i className="bi bi-box-arrow-right"  ></i></Link>
                     <DisplayToggle />
                 </div>
