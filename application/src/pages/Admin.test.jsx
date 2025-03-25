4ca9e30967b565a1d02d812fc1da19da2f5bda12
Crystal <crystaltsy121@gmail.com>
2025-03-25 02:05:54 +0000

    complete new admin page tests

--- application/src/pages/Admin.test.jsx
+++ application/src/pages/Admin.test.jsx
@@ -425,16 +425,13 @@ describe('Admin Component', () => {
             fireEvent.click(screen.getByText('Tasks'));
             
             await waitFor(() => {
-                // Initially selectedTask should be null
                 expect(screen.getByTestId('admin-task-form').getAttribute('data-selected-task')).toBe('null');
             });
             
-            // Select a task
             const editButtons = screen.getAllByText('Edit');
             fireEvent.click(editButtons[0]);
             
             await waitFor(() => {
-                // Verify the selectedTask prop is updated correctly
                 const selectedTask = JSON.parse(screen.getByTestId('admin-task-form').getAttribute('data-selected-task'));
                 expect(selectedTask).toEqual(mockTasks[0]);
             });
